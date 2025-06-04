0a18abcd4ec246444d57df3b26439b7c0c821f6b
tusokvar <tuslecha.il@gmail.com>
2025-06-04 22:57:05 +0300

    Update App.jsx

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import Home from './Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Payment from './pages/Payment';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/payment" element={<Payment />} />
+      </Routes>
+    </Router>
   );
 }
 
