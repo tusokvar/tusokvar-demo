0cb878ccf0d90ff4235941caa04a017542d45a6c
tusokvar <tuslecha.il@gmail.com>
2025-06-04 23:39:40 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Payment from './pages/Payment';
+
+import Home from './Home';
+import Payment from './Payment';
+import FlightResults from './FlightResults';
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/payment" element={<Payment />} />
+        <Route path="/results" element={<FlightResults />} />
       </Routes>
     </Router>
   );
