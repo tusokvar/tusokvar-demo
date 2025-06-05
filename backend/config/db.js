017e7ad9ca5de605709cf68ea0ddb188ce220af9
tusokvar <tuslecha.il@gmail.com>
2025-06-05 22:21:40 +0300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,11 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
+    // הדגלים הבאים מומלצים לאובדן תקלות
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
+      useUnifiedTopology: true
     });
-
     console.log(`✅ MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ MongoDB connection error:`, error);
