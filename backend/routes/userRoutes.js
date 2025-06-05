599ae6363cc51e2fdd47a35b7e5b570b0c99428c
tusokvar <tuslecha.il@gmail.com>
2025-06-05 18:20:28 +0300

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const {
-  registerUser,
-  loginUser,
-  getUserProfile
-} = require('../controllers/userController');
+const { registerUser, loginUser } = require('../controllers/userController');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.get('/profile', getUserProfile);
 
 module.exports = router;
