a58e0d54812ea214ba937bce403781a72aa8af2c
tusokvar <tuslecha.il@gmail.com>
2025-06-05 22:22:55 +0300

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,24 +1,42 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
-const userSchema = mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
   },
-  { timestamps: true }
-);
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  // ניתן להוסיף שדות נוספים כרצונכם...
+}, {
+  timestamps: true
+});
+
+// לפני השמירה, מצפינים את הסיסמה
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// שיטה לבדיקת סיסמה בהתחברות
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
-
 export default User;
