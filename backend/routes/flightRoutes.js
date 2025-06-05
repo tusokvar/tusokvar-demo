9db50a5976d9f08378de406f4ab21e38c0ddccbc
tusokvar <tuslecha.il@gmail.com>
2025-06-05 18:52:07 +0300

    Update flightRoutes.js

--- backend/routes/flightRoutes.js
+++ backend/routes/flightRoutes.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { searchFlights } = require('../controllers/flightController');
+const {
+  searchFlights,
+  getFlightDetails
+} = require('../controllers/flightController');
 
+// חיפוש טיסות
 router.get('/search', searchFlights);
+
+// קבלת פרטי טיסה לפי מזהה
+router.get('/:id', getFlightDetails);
 
 module.exports = router;
