54388f92d1ae222e4fd5b56e265d83a371bf4211
tusokvar <tuslecha.il@gmail.com>
2025-06-05 16:17:36 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,34 @@
 const express = require('express');
-const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const flightRoutes = require('./routes/flightRoutes');
 const userRoutes = require('./routes/userRoutes');
+const cors = require('cors');
+const path = require('path');
 
 dotenv.config();
 connectDB();
 
 const app = express();
-
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// API Routes
+// ראוטים
 app.use('/api/flights', flightRoutes);
 app.use('/api/users', userRoutes);
 
-// Root route for testing
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+// הפניית פרונטאנד אם הוא באותו שרת (Production)
+if (process.env.NODE_ENV === 'production') {
+  const __dirname1 = path.resolve();
+  app.use(express.static(path.join(__dirname1, '/frontend/build')));
 
-// Start server
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname1, 'frontend', 'build', 'index.html'))
+  );
+}
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`🚀 Server is running on port ${PORT}`);
 });
