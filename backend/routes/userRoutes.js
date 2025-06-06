9f70b5eb1f1892999fce42a23e03491fea801a79
tusokvar <tuslecha.il@gmail.com>
2025-06-06 16:58:44 +0300

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,10 @@
-// backend/routes/userRoutes.js
 const express = require('express');
 const router = express.Router();
-const { registerUser, authUser } = require('../controllers/userController');
+const { registerUser, loginUser, getUserProfile } = require('../controllers/userController');
+const { protect } = require('../middleware/authMiddleware');
 
-// רישום משתמש חדש
 router.post('/register', registerUser);
-
-// התחברות משתמש (login)
-router.post('/login', authUser);
+router.post('/login', loginUser);
+router.get('/profile', protect, getUserProfile);
 
 module.exports = router;
