e65be96196f72cdc236c2bee58c0c1ad08803981
tusokvar <tuslecha.il@gmail.com>
2025-06-06 17:59:25 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// frontend/src/app.jsx
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import FlightResults from "./pages/FlightResults.jsx";
+import Payment from "./pages/Payment.jsx";
+import ChatWidget from "./components/chatwidget.jsx";
+import "./pages/Home.css";
+import "./pages/FlightResults.css";
+import "./pages/Payment.css";
+import "./components/chatwidget.css";
 
-import Home from './pages/Home';
-import Payment from './pages/Payment';
-import FlightResults from './pages/FlightResults';
-
-import './pages/Home.css';
-import './pages/Payment.css';
-
-function App() {
+const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path="/results" element={<FlightResults />} />
-      </Routes>
-    </Router>
+    <BrowserRouter>
+      <div className="app-container" dir="rtl">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/results" element={<FlightResults />} />
+          <Route path="/payment" element={<Payment />} />
+        </Routes>
+        <ChatWidget />
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
