92f8a3208f7b650f57b8373297258de45f4c853a
tusokvar <tuslecha.il@gmail.com>
2025-06-06 16:57:47 +0300

    Update routes.js

--- backend/api/routes.js
+++ backend/api/routes.js
@@ -1,22 +1,12 @@
-// backend/api/routes.js
 const express = require('express');
 const router = express.Router();
 
-// ייבוא כל הנתיבים
 const userRoutes = require('../routes/userRoutes');
 const flightRoutes = require('../routes/flightRoutes');
 const emailRoutes = require('../routes/emailRoutes');
 
-// אופציית רכיבי תשלום אם יש צורך
-// const paymentRoutes = require('../routes/paymentRoutes');
-// const bookingRoutes = require('../routes/bookingRoutes');
-
 router.use('/users', userRoutes);
 router.use('/flights', flightRoutes);
 router.use('/emails', emailRoutes);
-
-// במידת הצורך נוסיף כאן עוד נתיבים
-// router.use('/payments', paymentRoutes);
-// router.use('/bookings', bookingRoutes);
 
 module.exports = router;
