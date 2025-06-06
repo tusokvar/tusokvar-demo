83d8134594f11210f7825365e1312625af321ccc
tusokvar <tuslecha.il@gmail.com>
2025-06-06 17:06:21 +0300

    Update and rename backend/emailService.js to backend/controllers/emailService.js

---
+++ backend/controllers/emailService.js
@@ -0,0 +1,18 @@
+const nodemailer = require('nodemailer');
+
+exports.sendEmail = async (to, subject, text) => {
+    const transporter = nodemailer.createTransport({
+        service: process.env.EMAIL_SERVICE,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+
+    await transporter.sendMail({
+        from: process.env.EMAIL_USER,
+        to,
+        subject,
+        text
+    });
+};
