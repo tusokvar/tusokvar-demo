c0e712529f0a653ddbf71682faf1eda1c474b7a3
tusokvar <tuslecha.il@gmail.com>
2025-06-06 17:18:04 +0300

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,30 +1,4 @@
 const User = require('../models/user');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-
-exports.registerUser = async (req, res) => {
-    const { email, password } = req.body;
-    const userExists = await User.findOne({ email });
-
-    if (userExists) return res.status(400).json({ message: 'User already exists' });
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await User.create({ email, password: hashedPassword });
-
-    res.status(201).json({ email: user.email });
-};
-
-exports.loginUser = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-
-    if (!user || !(await bcrypt.compare(password, user.password)))
-        return res.status(401).json({ message: 'Invalid credentials' });
-
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
-
-    res.json({ token });
-};
 
 exports.getUserProfile = async (req, res) => {
     const user = await User.findById(req.user.id).select('-password');
