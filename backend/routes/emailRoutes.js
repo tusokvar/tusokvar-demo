9c83064265f79fe85c69f913442cb65e8a0fb074
tusokvar <tuslecha.il@gmail.com>
2025-06-08 02:54:10 +0300

    Update and rename emailRoutes.js to paymentRoutes.js

--- backend/routes/emailRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { sendBookingConfirmation } = require('../controllers/emailController');
-
-router.post('/send-confirmation', sendBookingConfirmation);
-
-module.exports = router;
---
+++ backend/routes/paymentRoutes.js
@@ -0,0 +1,9 @@
+// backend/routes/paymentRoutes.js
+const express = require('express');
+const router = express.Router();
+const { processPayment } = require('../controllers/paymentController');
+const { protect } = require('../middleware/authMiddleware');
+
+router.post('/', protect, processPayment);
+
+module.exports = router;
