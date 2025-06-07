ee00f0d5474a1ed10b301bead626b2854760971b
tusokvar <tuslecha.il@gmail.com>
2025-06-08 02:49:11 +0300

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,8 +1,11 @@
+// backend/models/user.js
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-}, { timestamps: true });
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('User', UserSchema);
