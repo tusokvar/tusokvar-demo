ef83a678f908d00319a0ae329f4c59b3934aad49
tusokvar <tuslecha.il@gmail.com>
2025-06-08 15:05:16 +0300

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,9 +1,12 @@
-// frontend/src/utils/api.js
 import axios from 'axios';
-import { BACKEND_URI } from '../config';
+import { BACKEND_URI } from './config';
 
-const api = axios.create({
-  baseURL: `${BACKEND_URI}/api`,
-});
-
-export default api;
+export const searchFlights = async (searchParams) => {
+  try {
+    const response = await axios.post(`${BACKEND_URI}/api/flights/search`, searchParams);
+    return response.data;
+  } catch (error) {
+    console.error("API error:", error);
+    throw error;
+  }
+};
