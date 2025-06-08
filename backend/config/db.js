7f27db0dedeb6f138a38aaf93b8da3b15c1ee6fb
tusokvar <tuslecha.il@gmail.com>
2025-06-08 03:33:02 +0300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,13 +3,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
-  } catch (error) {
-    console.error('MongoDB connection error:', error.message);
+  } catch (err) {
+    console.error(err.message);
     process.exit(1);
   }
 };
