b0f51ad1ae92612e80d17801c457e54c45eb902d
tusokvar <tuslecha.il@gmail.com>
2025-06-08 03:25:00 +0300

    Update flightRoutes.js

--- backend/routes/flightRoutes.js
+++ backend/routes/flightRoutes.js
@@ -1,4 +1,3 @@
-// backend/routes/flightRoutes.js
 const express = require('express');
 const router = express.Router();
 const { searchFlights, bookFlight } = require('../controllers/flightController');
