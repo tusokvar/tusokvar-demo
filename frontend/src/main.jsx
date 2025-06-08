4a330819b1973ea7efc5aaa639f6fbf35d241d82
tusokvar <tuslecha.il@gmail.com>
2025-06-08 04:52:12 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,11 @@
-// frontend/src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './app.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
