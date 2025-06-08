a6972362864f4ea06851c46b6089ec210343adf7
tusokvar <tuslecha.il@gmail.com>
2025-06-08 03:39:39 +0300

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,2 @@
-export const API_URL = "https://tusokvar-demo.onrender.com/api";
+// frontend/src/config.js
+export const BACKEND_URI = import.meta.env.VITE_BACKEND_URI || 'https://tusokvar-demo.onrender.com';
