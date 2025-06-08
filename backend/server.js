a1e4536c43b92754aadc457c4c660f5e87ab5659
tusokvar <tuslecha.il@gmail.com>
2025-06-08 14:23:05 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,29 +4,22 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const routes = require('./api/routes');
 
-// Load environment variables
 dotenv.config();
-
-// Connect to MongoDB
 connectDB();
 
 const app = express();
 
-// Middleware
-app.use(cors({ origin: process.env.FRONTEND_URI }));
+const corsOptions = {
+    origin: process.env.FRONTEND_URI, 
+    optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-// Basic API route for testing
-app.get('/api', (req, res) => {
-  res.send('API is running');
-});
-
-// Main API routes
 app.use('/api', routes);
 
-// Server start
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
