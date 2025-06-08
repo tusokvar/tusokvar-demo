fe84a44aad11f0404162c20e11f8d7b5d46f14e6
tusokvar <tuslecha.il@gmail.com>
2025-06-08 03:07:14 +0300

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,27 +1,23 @@
-const User = require('../models/user');
-const bcrypt = require('bcryptjs');
+// backend/controllers/authController.js
 const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
-exports.registerUser = async (req, res) => {
-    const { email, password } = req.body;
-    const userExists = await User.findOne({ email });
+exports.protect = async (req, res, next) => {
+  let token;
 
-    if (userExists) return res.status(400).json({ message: 'User already exists' });
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await User.create({ email, password: hashedPassword });
+  if (!token) {
+    return res.status(401).json({ msg: 'Not authorized, no token provided' });
+  }
 
-    res.status(201).json({ email: user.email });
-};
-
-exports.loginUser = async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-
-    if (!user || !(await bcrypt.compare(password, user.password)))
-        return res.status(401).json({ message: 'Invalid credentials' });
-
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
-
-    res.json({ token });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select('-password');
+    next();
+  } catch (error) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
 };
