ab379975c560cb14fb05b782de52ae57fdbfe409
tusokvar <tuslecha.il@gmail.com>
2025-06-08 15:06:10 +0300

    Update app.jsx

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import FlightSearch from './pages/FlightSearch';
 import FlightResults from './pages/FlightResults';
 import Payment from './pages/Payment';
 import Booking from './pages/Booking';
-import ChatWidget from './components/ChatWidget';
+import FlightSearch from './pages/FlightSearch';
+import ChatWidget from './components/chatwidget';
 
-function App() {
-  return (
-    <Router>
-      <ChatWidget />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<FlightSearch />} />
-        <Route path="/results" element={<FlightResults />} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path="/booking" element={<Booking />} />
-      </Routes>
-    </Router>
-  );
-}
+const App = () => (
+  <Router>
+    <ChatWidget />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/flight-results" element={<FlightResults />} />
+      <Route path="/payment" element={<Payment />} />
+      <Route path="/booking" element={<Booking />} />
+      <Route path="/flight-search" element={<FlightSearch />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
