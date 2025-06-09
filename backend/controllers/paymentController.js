748c7a78344777b871efe88064ef3ff69254544a
tusokvar <tuslecha.il@gmail.com>
2025-06-09 22:55:18 +0300

    Update paymentController.js

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -4,15 +4,20 @@ exports.processPayment = async (req, res) => {
   const { amount, paymentMethodId } = req.body;
 
   try {
- await stripe.paymentIntents.create({
-  amount: Math.round(amount * 100),
-  currency: 'eur',
-  payment_method: paymentMethodId,
-  confirmation_method: 'automatic',
-  confirm: true,
-  return_url: "https://tusokvar-demo-1.onrender.com/payment-success",
-});
-    res.json({ success: true, paymentIntent });
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(amount * 100),
+      currency: 'eur',
+      payment_method: paymentMethodId,
+      confirmation_method: 'manual',
+      confirm: true,
+      return_url: 'https://tusokvar-demo.onrender.com/payment-success'
+    });
+
+    res.json({
+      success: true,
+      clientSecret: paymentIntent.client_secret,
+    });
+
   } catch (error) {
     console.error('Stripe Error:', error);
     res.status(500).json({ success: false, error: error.message });
