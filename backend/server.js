824a57a140fd62e2e00fb47704cfdc9e7e2f4860
tusokvar <tuslecha.il@gmail.com>
2025-06-10 00:04:47 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
 const connectDB = require('./config/db');
 const routes = require('./api/routes');
 
@@ -10,14 +11,23 @@ connectDB();
 const app = express();
 
 const corsOptions = {
-    origin: process.env.FRONTEND_URI, 
+    origin: process.env.FRONTEND_URI,
     optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
 
+// נתיבי ה-API
 app.use('/api', routes);
+
+// הגדרת נתיבים סטטיים לקבצי React שנבנו
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+// ניתוב כללי ל-React Router
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
