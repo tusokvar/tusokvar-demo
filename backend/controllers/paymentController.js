6d5f3240195fb9e97b431c4b667477ae52d33f8d
tusokvar <tuslecha.il@gmail.com>
2025-06-09 21:49:51 +0300

    Update paymentController.js

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -4,15 +4,18 @@ exports.processPayment = async (req, res) => {
   const { amount, paymentMethodId } = req.body;
 
   try {
-    const paymentIntent = await stripe.paymentIntents.create({
-      amount: Math.round(amount * 100),
-      currency: 'eur',
-      payment_method: paymentMethodId,
-      confirm: true,
-      automatic_payment_methods: { enabled: true },
-      return_url: 'https://tusokvar-demo-1.onrender.com/payment-success' 
-    });
-
+ const paymentIntent = await stripe.paymentIntents.create({
+  amount: Math.round(amount * 100),
+  currency: 'eur',
+  payment_method: paymentMethodId,
+  confirmation_method: 'automatic',
+  confirm: true,
+  return_url: 'https://tusokvar-demo-1.onrender.com/payment-success',
+  automatic_payment_methods: {
+    enabled: true,
+    allow_redirects: 'always' // יאפשר ל־Stripe לבצע Redirect במידה ודרוש אימות נוסף
+  }
+});
     res.json({ success: true, paymentIntent });
   } catch (error) {
     console.error('Stripe Error:', error);
