bbb1d73e72914aadbf056a6b7d9e0da75ba5f962
tusokvar <tuslecha.il@gmail.com>
2025-06-09 23:16:28 +0300

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { BACKEND_URI } from './config';
 
 const api = axios.create({
-  baseURL: `${BACKEND_URI}/api`,
+  baseURL: `${BACKEND_URI}`,
 });
 
 export default api;
