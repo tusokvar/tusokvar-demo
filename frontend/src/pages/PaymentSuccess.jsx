d35f920cb1b14a8e59d242cb57f357a55b71a8fa
tusokvar <tuslecha.il@gmail.com>
2025-06-09 20:27:08 +0300

    Create PaymentSuccess.jsx

---
+++ frontend/src/pages/PaymentSuccess.jsx
@@ -0,0 +1,13 @@
+// src/pages/PaymentSuccess.jsx
+import React from 'react';
+
+const PaymentSuccess = () => {
+  return (
+    <div>
+      <h2>התשלום התקבל בהצלחה!</h2>
+      <p>תודה על ההזמנה. אישור ההזמנה נשלח למייל שלך.</p>
+    </div>
+  );
+};
+
+export default PaymentSuccess;
