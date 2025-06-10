6f1816e59cad1ec4fe064ce531aeecff0fb1aa3d
tusokvar <tuslecha.il@gmail.com>
2025-06-10 21:58:56 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import fs from 'fs';
 
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
   build: {
     outDir: 'dist',
+  },
+  writeBundle() {
+    const redirectsSrc = resolve(__dirname, 'public/_redirects');
+    const redirectsDest = resolve(__dirname, 'dist/_redirects');
+    if (fs.existsSync(redirectsSrc)) {
+      fs.copyFileSync(redirectsSrc, redirectsDest);
+    }
   },
 });
